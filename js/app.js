8fa0c577181bd6498e57c86c2bc538150b496c36
Pu Xu <ptx.pluto@gmail.com>
2019-12-08 00:05:03 +0800

    better font

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@
 
 // 1. Define route components.
 // These can be imported from other files
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
+const Profile = { template: '<div>profile</div>' };
+const Recommandation = { template: '<div>recommandation</div>' };
 
 // 2. Define some routes
 // Each route should map to a component. The "component" can
@@ -12,22 +12,22 @@ const Bar = { template: '<div>bar</div>' }
 // `Vue.extend()`, or just a component options object.
 // We'll talk about nested routes later.
 const routes = [
-    { path: '/foo', component: Foo },
-    { path: '/bar', component: Bar }
-]
+    { path: '/profile', component: Profile },
+    { path: '/recommandation', component: Recommandation }
+];
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = new VueRouter({
     routes // short for `routes: routes`
-})
+});
 
 // 4. Create and mount the root instance.
 // Make sure to inject the router with the router option to make the
 // whole app router-aware.
 const app = new Vue({
     router
-}).$mount('#app')
+}).$mount('#app');
 
 // Now the app has started!
